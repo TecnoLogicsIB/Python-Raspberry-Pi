7dd4fc8d984abea7f8655ff88ae47ff6c20f853f
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2023-05-01 20:42:37 +0200

    Create BMP280_nubol.py

---
+++ BMP280/BMP280_nubol.py
@@ -0,0 +1,2 @@
+logger.info(str(temperatura) + "|" + str(pressio)) #traspas de dades en format string
+      print ("temperatura: %s C \t \t pressio: %s hPa \t \t altitud: %s m" % (temperatura, pressio, altitud))
